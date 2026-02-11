29c9cd3b7aa1f9503f9918e085397d168d4d0e0b
Sushan-KS <sushanks2004@gmail.com>
2026-02-11 14:47:30 +0530

    the second commit actually
    the first one is stored in the md file and this is the second commit to the first file

---
+++ App.js
@@ -0,0 +1,69 @@
+import { useState } from "react";
+
+
+function App() {
+
+  const [email, setEmail]=useState("");
+  const[password, setPassword]=useState("");
+
+  const handleLogin =()=>
+  {
+    if(email==="suhan123@gmail.com" && password==="1234")
+    {
+      alert("login successful");
+    }
+    else 
+    {
+      alert("invalid credentials");
+    }
+  }
+
+  return (
+    <div >
+    <h2>Login Form</h2>
+
+    <input
+      type='text'
+      placeholder="enter email"
+      value={email}
+      onChange={(e)=> setEmail(e.target.value)}
+    />
+    <br />
+    <br />
+    <input
+      type="text"
+      placeholder="Password"
+      value={password}
+      onChange={(e)=>setPassword(e.target.value)}
+    />
+    <br />
+    <br />
+
+    <button onClick={handleLogin}>Login</button>
+    
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Login</h1>
+
+//       <input type="text" placeholder="Enter Username" />
+//       <br /><br />
+
+//       <input type="password" placeholder="Enter Password" />
+//       <br /><br />
+
+//       <button>Login</button>
+//     </div>
+//   );
+// }
+
+// export default App;
