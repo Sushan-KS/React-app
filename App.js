3188e70a1780612253b90188af67c4951bc4c17b
Sushan-KS <sushanks2004@gmail.com>
2026-02-12 11:08:54 +0530

    replace everything inside app.js
    when i needed to add the userouter so i was told to remove everything and replace it with the new file and had to create 2 seperate files named login.js and dashboard.js

--- App.js
+++ App.js
@@ -1,77 +1,16 @@
-import { useState } from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Login";
+import Dashboard from "./Dashboard";
 
 function App() {
-
-  const [email, setEmail]=useState("");
-  const [password, setPassword]=useState("");
-  const [isLoggedIn, setIsLoggedIn]= useState(false); 
-
-  const handleLogin =()=>
-  {
-    if(email.includes("@")
-    {
-     setIsLoggedIn(true);
-    }
-    else 
-    {
-      alert("enter valid email");
-    }
-  }
-
   return (
-    <div>
-  {isLoggedIn ?(
-    <h1>Welcome to Dashboard</h1>
-    ):(
-    
-    <div >
-    <h2>Login Form</h2>
-
-    <input
-      type='text'
-      placeholder="enter email"
-      value={email}
-      onChange={(e)=> setEmail(e.target.value)}
-    />
-    <br />
-    <br />
-    <input
-      type="text"
-      placeholder="Password"
-      value={password}
-      onChange={(e)=>setPassword(e.target.value)}
-    />
-    <br />
-    <br />
-
-    <button onClick={handleLogin}>Login</button>
-    
-    </div>
-        )}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
-
-
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <h1>Login</h1>
-
-//       <input type="text" placeholder="Enter Username" />
-//       <br /><br />
-
-//       <input type="password" placeholder="Enter Password" />
-//       <br /><br />
-
-//       <button>Login</button>
-//     </div>
-//   );
-// }
-
-// export default App;
