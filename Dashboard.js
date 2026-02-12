9a8aca65ae7c1e31798e2cc7976d088499ab75ca
Sushan-KS <sushanks2004@gmail.com>
2026-02-12 12:49:31 +0530

    Create Dashboard.js
    for routing purpose

---
+++ Dashboard.js
@@ -0,0 +1,11 @@
+function Dashboard()
+{
+    return (
+        <div>
+        <h2>Dashboard</h2>
+        <p>You have succesfully logged in.</p>
+        </div>
+    );
+    
+}
+export default Dashboard;
